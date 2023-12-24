44827280482a34b2f0be5e1756938d75940b442b
kjn01 <91856362+kjn01@users.noreply.github.com>
2023-12-24 14:18:06 -0600

    add build and remove request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,16 @@ import Board from './components/Board';
 export default function App() {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
+        {/* <p>{!data ? "Loading..." : data}</p> */}
         <Board />
       </header>
     </div>
